d6ce364abc9ff0a4ca0c553a15225e107eefe5b0
cillahh <cillah164@naver.com>
2024-05-28 04:12:53 +0900

    Update drag_select_grid_view.dart

--- lib/src/drag_select_grid_view/drag_select_grid_view.dart
+++ lib/src/drag_select_grid_view/drag_select_grid_view.dart
@@ -238,7 +238,7 @@ class DragSelectGridViewState extends State<DragSelectGridView>
       _selectionManager.selectedIndexes = controller.value.selectedIndexes;
       if (widget.allSelection) {
         for (int i = 0; i < widget.itemCount!; i++) {
-          controller.value.selectedIndexes.add(i);
+          _selectionManager.selectedIndexes.add(i);
         }
       }
     }
