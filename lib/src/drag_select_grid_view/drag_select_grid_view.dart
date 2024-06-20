ef472e464c290cef5745cf194312e5fc1bfcac77
cillahh <cillah164@naver.com>
2024-06-20 23:11:22 +0900

    Update drag_select_grid_view.dart

--- lib/src/drag_select_grid_view/drag_select_grid_view.dart
+++ lib/src/drag_select_grid_view/drag_select_grid_view.dart
@@ -282,7 +282,7 @@ class DragSelectGridViewState extends State<DragSelectGridView>
           clipBehavior: widget.clipBehavior,
           itemBuilder: (context, index) {
             return IgnorePointer(
-              ignoring: false,
+              ignoring: isSelecting,
               child: Selectable(
                 index: index,
                 onMountElement: _elements.add,
